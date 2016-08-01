960453becd9d728d57e8ceb907fb3c152b01810c
ys <lixhuang@umich.edu>
2016-07-31 20:35:21 -0700

    finish vga buffer

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -33,18 +33,19 @@ impl ColorCode
 }
 
 #[repr(C)]
+#[derive(Clone, Copy)]
 struct ScreenChar
 {
 	ascii_char: u8,
 	color_code: ColorCode,
 }
 
-const BUFFER_HIGHT: usize = 25;
+const BUFFER_HEIGHT: usize = 25;
 const BUFFER_WIDTH: usize = 80;
 
 struct Buffer
 {
-	chars: [[ScreenChar;BUFFER_WIDTH];BUFFER_HIGHT],
+	chars: [[ScreenChar;BUFFER_WIDTH];BUFFER_HEIGHT],
 }
 
 use core::ptr::Unique;
@@ -56,16 +57,20 @@ pub struct Writer
 	buffer: Unique<Buffer>,
 }
 
-impl Writer
-{
-	pub fn write_string(&mut self, s: &str)
+impl ::core::fmt::Write for Writer {
+	// add code here
+	fn write_str(&mut self, s: &str) -> ::core::fmt::Result
 	{
 		for byte in s.bytes()
 		{
 			self.write_byte(byte);
 		}
+		Ok(())
 	}
+}
 
+impl Writer
+{
 	pub fn write_byte(&mut self, byte: u8)
 	{
 		match byte
@@ -76,7 +81,7 @@ impl Writer
 					self.new_line();
 				}
 			
-				let row = BUFFER_HIGHT - 1;
+				let row = BUFFER_HEIGHT - 1;
 				let col = self.column_position;
 
 				self.buffer().chars[row][col] = ScreenChar{
@@ -95,6 +100,62 @@ impl Writer
 
 	fn new_line(&mut self)
 	{
+		for row in 0..(BUFFER_HEIGHT - 1)
+		{
+			let buffer = self.buffer();
+			buffer.chars[row] = buffer.chars[row + 1];
+		}
+		self.clear_row(BUFFER_HEIGHT - 1);
+		self.column_position = 0;
+	}
 
+	fn clear_row(&mut self, row: usize)
+	{
+		let blank = ScreenChar {
+        	ascii_char: b' ',
+        	color_code: self.color_code,
+    	};
+    	self.buffer().chars[row] = [blank; BUFFER_WIDTH];
 	}
 }
+
+use spin::Mutex;
+
+pub static WRITER: Mutex<Writer> = Mutex::new(Writer {
+    column_position: 0,
+    color_code: ColorCode::new(Color::LightGreen, Color::Black),
+    buffer: unsafe { Unique::new(0xb8000 as *mut _) },
+});
+
+macro_rules! print {
+    ($($arg:tt)*) => ({
+        use core::fmt::Write;
+        let mut writer = $crate::vga_buffer::WRITER.lock();
+        writer.write_fmt(format_args!($($arg)*)).unwrap();
+    });
+}
+
+macro_rules! println {
+    ($fmt:expr) => (print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => (print!(concat!($fmt, "\n"), $($arg)*));
+}
+
+
+pub fn clear_screen() {
+    for _ in 0..BUFFER_HEIGHT {
+        println!("");
+    }
+}
+
+/*
+pub extern fn print_smth()
+{
+	use core::fmt::Write;
+	let mut writer = Writer{
+		column_position: 0,
+		color_code: ColorCode::new(Color::White, Color::Blue),
+		buffer: unsafe{ Unique::new(0xb8000 as *mut _)},
+	};
+	write!(writer, "print out {}, {} and {}, {}", "a", "4", "hello", "4.4");
+}
+*/
